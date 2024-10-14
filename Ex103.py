8bced715d837d199fce96b766a6d14adc11211eb
Lê Hoài Thành Danh <danhlht234111e@st.uel.edu.vn>
2024-10-15 00:24:51 +0700

    2

--- Ex103.py
+++ Ex103.py
@@ -1,3 +1,4 @@
+m = int(input('Nhap chieu cao: '))
 def pascal(m):
     n = 2 * m + 1
     mid = n // 2
@@ -17,7 +18,48 @@ def pascal(m):
                 print("  ", end="")
         print()
 
-m = int(input('Nhap chieu cao: '))
 pascal(m)
+def pascal2(m):
+    n = 2*m+1
+    mid = n // 2
+    A = [[0 for _ in range(m)] for _ in range(n)]
+
+    A[mid][0] = 1
+
+    for j in range(1, m): #cot :m
+        for i in range(n): #dong:n
+            if i < n - 1:
+                A[i][j] += A[i - 1][j - 1]
+                A[i][j] += A[i + 1][j - 1]
+
+    for i in range(n):
+        for j in range(m):
+            if A[i][j] != 0:
+                print(f"{A[i][j]} ", end="")
+            else:
+                print("  ", end="")
+        print()
+
+pascal2(m)
+def pascal3(m):
+    n = 2*m+1
+    mid = n // 2
+    A = [[0 for _ in range(m)] for _ in range(n)]
+    A[mid][m-1] = 1
+    for j in range(m, 0, -1): #cot :m
+        for i in range(n-1): #dong:n
+            if j < m:
+                A[i][j-1] += (A[i - 1][j])
+                A[i][j-1] += (A[i + 1][j])
+            if A[i][j-1] > 1:
+                A[i][j-1] +=1
+    for i in range(n):
+        for j in range(m):
+            if A[i][j] != 0:
+                print(f"{A[i][j]:2d}", end="")
+            else:
+                print("  ", end="")
+        print()
+pascal3(m)
 
 
